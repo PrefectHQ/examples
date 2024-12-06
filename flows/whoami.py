f50abe4febfdf7644ac4d7f77ac816f79e5137ee
Emil Christensen <emil@prefect.io>
2024-12-06 15:14:05 -0500

    Adds note about deployment of whoami flow

--- flows/whoami.py
+++ flows/whoami.py
@@ -2,6 +2,20 @@
 This flow will log information about the current environment. Use it to
 diagnose issues with your environment, especially when deploying to
 infrastructure.
+
+The flow has no dependencies and can be deployed from its public source:
+
+```python
+from prefect import flow
+
+flow.from_source(
+    source="https://github.com/PrefectHQ/examples.git",
+    entrypoint=flows/whoami.py:whoami,
+).deploy(
+    name="default",
+    work_pool_name="my-work-pool",
+)
+```
 """
 
 import sys
