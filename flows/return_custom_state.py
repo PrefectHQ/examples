24a3efc34d67c63262681adaa634c0cdd3ef3547
zzstoatzz <thrast36@gmail.com>
2025-02-05 13:06:43 -0600

    update and add example

--- flows/return_custom_state.py
+++ flows/return_custom_state.py
@@ -3,34 +3,26 @@
 # ///
 
 """
-Demonstrates directly returning a state. In this case, an AwaitingRetry state
-which causes a task to be retried after a delay.
+Demonstrates directly returning a state. In this case, we return a named Completed state
+to indicate that a task SKIPPED execution in a way that is not an error.
 """
 
-from datetime import datetime, timedelta, timezone
-
-from prefect import get_run_logger, task
-from prefect.context import get_run_context
-from prefect.states import AwaitingRetry
+from prefect import task
+from prefect.states import Completed
 
 
-@task
+def get_work_to_do() -> list[str]:
+    return []
+
+
+@task(log_prints=True)
 def main():
-    get_run_logger().info("Running main task")
+    work_to_do = get_work_to_do()
 
-    # In practice we might fetch a URL that is temporarily unavailable
-    # and retry after a number of seconds specified by a Retry-After header
-    retry_after = 10
+    if not work_to_do:
+        return Completed(name="SKIPPED", message="No work to do")
 
-    # Break out of the retry loop after the first run
-    ctx = get_run_context()
-    if ctx.task_run.run_count > 1:
-        return
-
-    # Schedule the task to run again in {retry_after} seconds
-    return AwaitingRetry(
-        scheduled_time=datetime.now(timezone.utc) + timedelta(seconds=retry_after)
-    )
+    print(f"Proceeding to do work: {work_to_do}")
 
 
 if __name__ == "__main__":
